a9eef783ee93dcd3fa0d9bfb179e19c4680ebe03
stassrv <stas.sirovui@gmailn.comgit>
2019-05-03 00:09:37 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,9 @@
+;(function() {
+	"use strict";
+	let btn = document.querySelector('.ba-menu-toggle');
+		let menu = document.querySelector('.ba-menu');
+		btn.addEventListener('click', function (e) {
+			menu.classList.toggle('open');
+		});
+})();
+
